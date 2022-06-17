e63719110d836b845a584bcf665c30562437c4b0
bimamahardikaaa <63089605+bimamahardikaaa@users.noreply.github.com>
2022-06-17 09:59:26 +0700

    Update finaltubesvisdat.py

--- finaltubesvisdat.py
+++ finaltubesvisdat.py
@@ -4,116 +4,115 @@
 # 2. Muhammad Rifqi Arrahim 1301190425 
 # 3. Aini Nasywa 130
 
-# -*- coding: utf-8 -*-
 import pandas as pd
 from bokeh.plotting import figure
-from bokeh.plotting import show
-from bokeh.io import curdoc
 from bokeh.models import ColumnDataSource, HoverTool, Select
-from bokeh.models.widgets import Tabs, Panel
 from bokeh.layouts import row, widgetbox
 from bokeh.palettes import Category20_16
-from bokeh.models.widgets import CheckboxGroup, Slider, RangeSlider, Tabs
+from bokeh.models.widgets import Tabs, Panel
+from bokeh.plotting import show
+from bokeh.io import curdoc
 from bokeh.layouts import column, row, WidgetBox
+from bokeh.models.widgets import CheckboxGroup, Slider, RangeSlider, Tabs
 
-# Read data
-indeks = pd.read_csv("./data/covid_19_indonesia_time_series_all.csv")
+# Melakukan pembacaan data
+indekscovid = pd.read_csv("./data/covid19indonesia.csv")
 
-# Mengubah nilai kolom date dari object menjadi datetime
-indeks["Date"] = pd.to_datetime(indeks["Date"])
-indeks
+# Mengubah nilai dari kolom date dari object menjadi datetime
+indekscovid["Date"] = pd.to_datetime(indekscovid["Date"])
+indekscovid
 
-# Mendrop baris indonesia
-datacovid = indeks[indeks["Location"].str.contains("Indonesia")==False]
+# Melakukan drop baris pada indonesia
+data = indekscovid[indekscovid["Location"].str.contains("Indonesia")==False]
 
 # Mengambil data yang diperlukan
-datacovid = datacovid[['Date', 'Location', 'Total Cases', 'Total Deaths', 'Total Recovered', 'Total Active Cases']]
-datacovid
+data = data[['Date', 'Location', 'Total Cases', 'Total Deaths', 'Total Recovered', 'Total Active Cases']]
+data
 
-# Membuat list untuk location
-lokasi = list(datacovid.Location.unique())
+# Membuat list untuk location yang ada
+lokasi = list(data.Location.unique())
 
-# Membuat list tiap kolom
-col_list = list(datacovid.columns)
+# Membuat list tiap kolom yang ada
+col_list = list(data.columns)
 
 # Method untuk pembuatan dataset yang akan di select nanti
-def buat_dataset(lokasi, feature):
-    x_list = []
-    y_list = []
+def buatdataset(lokasi, feature):
+    list_x = []
+    list_y = []
     colors = []
     labels = []
 
     for i, lokasi in enumerate(lokasi):
 
-        df = datacovid[datacovid['Location'] == lokasi].reset_index(drop = True)
+        op = data[data['Location'] == lokasi].reset_index(drop = True)
         
-        x = list(df['Date'])
-        y = list(df[feature])
+        x = list(op['Date'])
+        y = list(op[feature])
         
-        x_list.append(list(x))
-        y_list.append(list(y))
+        list_x.append(list(x))
+        list_y.append(list(y))
 
         colors.append(Category20_16[i])
         labels.append(lokasi)
 
-    new_src = ColumnDataSource(datacovid={'x': x_list, 'y': y_list, 'color': colors, 'label': labels})
+    new_src = ColumnDataSource(data={'x': list_x, 'y': list_y, 'color': colors, 'label': labels})
 
     return new_src
 
 # Method untuk pembuatan multiple line plot yang akan di select nanti
-def buat_plot(src, feature):
+def buatplot(src, feature):
     
-    p = figure(plot_width = 700, plot_height = 400, 
-            title = 'Covid19-Indonesia All Time Series',
+    c = figure(plot_width = 700, plot_height = 400, 
+            title = 'Covid19-Indonesia',
             x_axis_label = 'Date', y_axis_label = 'Feature Selected')
 
-    p.multi_line('x', 'y', color = 'color', legend_field = 'label', line_width = 2, source = src)
+    c.multi_line('x', 'y', color = 'color', legend_field = 'label', line_width = 2, source = src)
 
     tooltips = [
             ('Date','$x'),
             ('Total', '$y'),
            ]
            
-    p.add_tools(HoverTool(tooltips=tooltips)) #hover
+    c.add_tools(HoverTool(tooltips=tooltips)) # Melakukan hover
 
-    return p
+    return c
 
 # Method callback untuk interaktif checkbox
-def update_lokasi(attr, old, new):
+def updatelokasi(attr, old, new):
     lokasi_plot = [lokasi_selection.labels[i] for i in lokasi_selection.active]
 
-    new_src = buat_dataset(lokasi_plot, feature_select.value)
+    new_src = buatdataset(lokasi_plot, feature_select.value)
 
     src.data.update(new_src.data)
 
 # Method callback untuk interaktif dropdown
-def update_fitur(attr, old, new):
+def updatefitur(attr, old, new):
     lokasi_plot = [lokasi_selection.labels[i] for i in lokasi_selection.active]
     
     feature = feature_select.value
     
-    new_src = buat_dataset(lokasi_plot, feature)
+    new_src = buatdataset(lokasi_plot, feature)
 
     src.data.update(new_src.data)
 
-# Pembuatan checkboxgroup berdasarkan provinsi/lokasi
+# Pembuatan checkboxgroup berdasarkan pada provinsi/lokasi
 lokasi_selection = CheckboxGroup(labels=lokasi, active = [0])
-lokasi_selection.on_change('active', update_lokasi)
+lokasi_selection.on_change('active', updatelokasi)
 
-# Pembuatan fitur select dropdown
+# Pembuatan fitur select dropdown 
 feature_select = Select(options = col_list[2:], value = 'Total Cases', title = 'Feature Select')
-feature_select.on_change('value', update_fitur)
+feature_select.on_change('value', updatefitur)
 
 lokasi_now = [lokasi_selection.labels[i] for i in lokasi_selection.active]
 
-src = buat_dataset(lokasi_now, feature_select.value)
+src = buatdataset(lokasi_now, feature_select.value)
 
 # Pemanggilan method plot
-p = buat_plot(src, feature_select.value)
+c = buatplot(src, feature_select.value)
 
-# Pemasangan widget untuk interaktive visualisasi
+# Pemasangan widget untuk interaktive visualisasi data covid
 controls = WidgetBox(feature_select, lokasi_selection)
 
-layout = row(controls, p)
+layout = row(controls, c)
 
 curdoc().add_root(layout)
